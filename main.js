7cc2b9bf21773b4785a16376875c20e0bf658e6d
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-13 15:24:00 +0200

    to do list

---
+++ main.js
@@ -0,0 +1,37 @@
+const add$$ = document.querySelector(".btn-add");
+const list$$ = document.querySelector(".li-container");
+const ul$$ = document.querySelector("ul");
+const empty$$ = document.querySelector(".empty");
+const searchBar$$ = document.querySelector("input");
+
+
+function handleAdd (event){
+    event.preventDefault(); // Detener el comportamiento por defecto del botón
+    const li$$ = document.createElement("li");
+    const span$$ = document.createElement("span");
+    const btnRemove$$ = document.createElement("button");
+    
+    ul$$.appendChild(li$$);
+    li$$.appendChild(span$$);
+    li$$.appendChild(btnRemove$$);
+
+    span$$.textContent = searchBar$$.value; // Asignar el valor del input al texto del elemento de la lista
+    
+    btnRemove$$.classList = "btn-delete";
+    btnRemove$$.textContent = "X"
+    btnRemove$$.addEventListener("click", function() {
+        li$$.remove();
+        checkEmpty(); // verifica despues de cada elemento que se borra y si no queda ninguno, muestra el mensaje
+    });
+    checkEmpty(); // verifica si se añadio algo y se oculta
+}
+add$$.addEventListener("click", handleAdd);
+
+function checkEmpty (){
+    if (ul$$.children.length === 0){
+        empty$$.style.display = "block"; // Mostrar el elemento .empty
+    }
+    else {
+        empty$$.style.display = "none"; // Ocultar el elemento .empty
+    }
+}
